correct color and rotation

Computer-Vision-3D-Reconstruction-master/Computer-Vision-3D-Reconstruction-master/BackgroundSubtraction.py
@@ -46,7 +46,7 @@ def refine_mask(mask):
     # create round kernel and morph image
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (4, 4))
     mask = cv.GaussianBlur(mask, (3, 3), 0)
-    mask = cv.dilate(mask, kernel, iterations=1)
+    mask = cv.dilate(mask, kernel, iterations=2)
     # find contours
     contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     # draw the biggest contour
@@ -91,30 +91,38 @@ def getFrames(cam, num_frames=30):
         frames.append(img)
     return np.array(frames)
 
-
 def main():
     #generate background and sd images for each camera
-    # for i in range(4):
-    #     video = cv.VideoCapture('data/cam' + str(i + 1) + '/background.avi')
-    #     frames = getFrames(video)
-    #     background, sd = compose_background(frames)
-    #     cv.imwrite('data/cam' + str(i + 1) + '/background.png', background)
-    #     cv.imwrite('data/cam' + str(i + 1) + '/sd.png', sd)
+    for i in range(4):
+        video = cv.VideoCapture('data/cam' + str(i + 1) + '/background.avi')
+        frames = getFrames(video)
+        background, sd = compose_background(frames)
+        cv.imwrite('data/cam' + str(i + 1) + '/background.png', background)
+        cv.imwrite('data/cam' + str(i + 1) + '/sd.png', sd)
     #generate mask and frame images for each camera
     for i in range(4):
         background = cv.imread('data/cam' + str(i + 1) + '/background.png')
         sd = cv.imread('data/cam' + str(i + 1) + '/sd.png')
         video = cv.VideoCapture('data/cam' + str(i + 1) + '/video.avi')
-        video = cv.VideoCapture('data/cam' + str(i + 1) +'/video.avi')
+        frames = getFrames(video, num_frames=5)
+        masks = subtract_background(video, background, sd)
 
-        new_frames = subtract_background(video, background, sd)
-        # for x in range(len(new_frames)):
-        #     cv.imwrite('data/cam' + str(i+1) + '/masks/mask' + str(x) + '.png', new_frames[x])
-        store_masks(new_frames, i+1)
+        store_masks(masks, i + 1)
+        store_video('frames', frames, i + 1)
 
+
+def store_video(name, frames, cam_num):
+    # store frames in a avi file
+    height, width, _ = frames[0].shape
+    size = (width, height)
+    fourcc = cv.VideoWriter_fourcc(*'MJPG')
+    out = cv.VideoWriter('data/cam' + str(cam_num) + '/' + name + '.avi', fourcc, 15, size)
+    for i in range(len(frames)):
+        out.write(frames[i])
+    out.release()
 
 def store_masks(masks, cam_num):
-    #store masks in a avi file
+    # store masks in a avi file
     height, width = masks[0].shape
     size = (width, height)
     fourcc = cv.VideoWriter_fourcc(*'MJPG')

Computer-Vision-3D-Reconstruction-master/Computer-Vision-3D-Reconstruction-master/GetCameraIntrinsics.py
@@ -282,15 +282,11 @@ def calibrateCam(intrinsic, extrinsic, cam_string):
 
     R = cv.Rodrigues(rvecs)[0]
 
-
-    #rotete R by 90 degrees around z axis
-    R = R * np.matrix([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
-
+    R = R * numpy.matrix([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
     T = -numpy.matrix(R).T * numpy.matrix(tvecs)
 
     #flip the y and z axis and negate z of T to get the correct translation
-    T = np.matrix([[T[0, 0]], [-T[2, 0]], [T[1, 0]]])
-
+    # T = np.matrix([[T[0, 0]], [T[2, 0]], [T[1, 0]]])s
     print(f'extrinsics {cam_string}:\n rotation:{rvecs},\n translation:\n {tvecs}, \n rotation matrix:\n {R}, \n translation matrix:\n {T}')
 
     camXML = cv.FileStorage(f'data/{cam_string}/config.xml', cv.FileStorage_WRITE)

Computer-Vision-3D-Reconstruction-master/Computer-Vision-3D-Reconstruction-master/assignment.py
@@ -7,7 +7,7 @@ import cv2 as cv
 
 block_size = 1.0
 
-voxel_scale = 27
+voxel_scale = 23
 
 
 def getConfig(cam_string):
@@ -47,11 +47,7 @@ frames2 = load_avi(2, 'frames')
 frames3 = load_avi(3, 'frames')
 frames4 = load_avi(4, 'frames')
 
-video1 = cv.VideoCapture('data/cam1/background.avi')
-video2 = cv.VideoCapture('data/cam2/background.avi')
-video3 = cv.VideoCapture('data/cam3/background.avi')
-video4 = cv.VideoCapture('data/cam4/background.avi')
-videos = [video1, video2, video3, video4]
+
 
 voxelFrameIdx = 0
 
@@ -126,7 +122,7 @@ def set_voxel_positions(width, height, depth):
     print("frame: " + str(clicks))
     # get mask from every camera
     masks = [masks1.read()[1], masks2.read()[1], masks3.read()[1], masks4.read()[1]]
-    frames = [video1.read()[1], video2.read()[1], video3.read()[1], video4.read()[1]]
+    frames = [frames1.read()[1], frames2.read()[1], frames3.read()[1], frames4.read()[1]]
     clicks += 1
 
     # create a zerros array of the shape width, height, depth
@@ -148,17 +144,10 @@ def set_voxel_positions(width, height, depth):
 
     # filter count in shapes to only include voxels that are in all masks
     # inAllCams = (countInShapes == 4).nonzero()
-    # data = np.array(inAllCams[0], inAllCams[1], inAllCams[2]).T
-    inAllCams = np.where(countInShapes == 4, 1, 0)
-    # add voxels in countInShapes to data
-    for x in range(int(-width/2), int(width/2)):
-        for y in range(height):
-            for z in range(int(-depth/2), int(depth/2)):
-                if inAllCams[x][y][z] == 1:
-                    data.append([x, y, z])
     data2 = data.copy()
+    print("coloring model")
     colors = create_colors(data2, width, height, depth, colors)
-    print(data, len(colors))
+    print("done")
     return data, colors
 
 def create_colors(data, width, height, depth, colors):
@@ -202,7 +191,7 @@ def create_colors(data, width, height, depth, colors):
                     break
             if found:
                 if countOccluded[neighbor_index[0]][neighbor_index[1]][neighbor_index[2]] == 0:
-                   color[x][y][z] = [0,0,0]
+                   color[x][y][z] = [255,255,255]
                    countOccluded[x][y][z] += 1
 
     #filter countOccluded to only exclude voxels that are occluded by all cameras
@@ -215,16 +204,6 @@ def create_colors(data, width, height, depth, colors):
                 else:
                     result.append(color[x][y][z])
     return result
-
-
-
-
-
-
-
-
-
-
 
 
 def get_cam_positions():
@@ -240,6 +219,6 @@ def get_cam_rotation_matrices():
     cam_rotations = []
     for c in range(len(cam_angles)):
         #rotate 90 degrees around y axis
-        cam_rotations.append(glm.rotate(glm.mat4(glm.mat3(cam_angles[c])), glm.radians(90), glm.vec3(0, 0, 1)))
+        cam_rotations.append(glm.rotate(glm.mat4(glm.mat3(cam_angles[c])), glm.radians(-90), glm.vec3(0, 1, 0)))
 
     return cam_rotations